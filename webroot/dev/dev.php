ae49a227a879e37d3099a609741bc6bd5cdb8e20
Julien Flusin <julienflusin3@gmail.com>
2024-10-07 21:56:42 +0200

    remove php part and stuff

--- webroot/dev/dev.php
+++
@@ -1,6 +0,0 @@
-<?php
-    foreach($_POST as $key => $val) {
-        echo $key.': '.$val.'<br>';
-        $data[$key] = $val;
-        }
-?>
--- webroot/stats.php
+++
@@ -1,7 +0,0 @@
-<?php
-$counter_name = "counter.txt";
-$f = fopen($counter_name,"r");
-$counterVal = fread($f, filesize($counter_name));
-fclose($f);
-echo($counterVal.' Visiteurs');
-?>
